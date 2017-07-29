d7eb78ccff85f3ef3855ce3bc08bd0cbfbd1b64f
starsriver <starsriver@outlook.com>
2017-07-29 20:59:53 +0800

    update

---
+++ 6/Discriminating Digit.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from sklearn.datasets import load_digits
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.preprocessing import LabelBinarizer
+from NeuralNetwork import NeuralNetwork
+from sklearn.cross_validation import train_test_split
+
+digits = load_digits()
+X = digits.data
+Y = digits.target
+X -= X.min()
+X /= X.max()
+
+nn = NeuralNetwork([64, 100, 10], "logistic")
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y)
+labels_train = LabelBinarizer().fit_transform(Y_train)
+labels_test = LabelBinarizer().fit_transform(Y_test)
+print("start fitting")
+nn.fit(X_train, labels_train, epochs=3000)
+predictions = []
+for i in range(X_test.shape[0]):
+    o = nn.predict(X_test[i])
+    predictions.append(np.argmax(o))
+print(confusion_matrix(Y_test, predictions))
+print(classification_report(Y_test, predictions))
