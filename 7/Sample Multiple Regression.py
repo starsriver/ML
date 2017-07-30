c581e1cc92babbfb6b1e4211966d8dec2cb49869
starsriver <starsriver@outlook.com>
2017-07-30 16:06:53 +0800

    Add Sample Multiple Regression and dataset

---
+++ 7/Sample Multiple Regression.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from numpy import genfromtxt
+# import numpy as np
+from sklearn import linear_model
+
+dataPath = r"./Delivery.csv"
+deliveryData = genfromtxt(dataPath, delimiter=',')
+print "data"
+print deliveryData
+
+X = deliveryData[:, :-1]
+Y = deliveryData[:, -1]
+
+print("X: ", X)
+print("Y: ", Y)
+
+regr = linear_model.LinearRegression()
+regr.fit(X, Y)
+
+print("coefficients: ", regr.coef_)
+print("intercept: ", regr.intercept_)
+
+xPred = [102, 6]
+yPred = regr.predict(xPred)
+
+print("oredicted y: ", yPred)
