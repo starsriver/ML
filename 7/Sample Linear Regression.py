00187c32c1cc2cac09c4b70fe1c6965dd6baf9a1
starsriver <starsriver@outlook.com>
2017-07-30 15:28:54 +0800

    Sample Linear Regression

---
+++ 7/Sample Linear Regression.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+
+def fitSLR(x, y):
+    n = len(x)
+    dinominator = 0
+    numberator = 0
+    for i in range(0, n):
+        numberator += (x[i] - np.mean(x)) * (y[i] - np.mean(y))
+        dinominator += (x[i] - np.mean(x)) ** 2
+    b1 = numberator / float(dinominator)
+    b0 = np.mean(y) / float(np.mean(x))
+    return b0, b1
+
+
+def predict(x, b0, b1):
+    return b0 + x * b1
+
+
+x = [1, 3, 2, 1, 3]
+y = [14, 24, 18, 17, 27]
+b0, b1 = fitSLR(x, y)
+print("intercept: ", b0, " slope: ", b1)
+
+x_test = 6
+
+y_test = predict(6, b0, b1)
+
+print("y_test: ", y_test)
