9681d108a41e11c29379ff9b9c2a739bdd9e1fa0
starsriver <starsriver@outlook.com>
2017-08-03 21:36:06 +0800

    update

--- 5/SVM Facial Recognition.py
+++ 5/SVM Facial Recognition.py
@@ -59,7 +59,7 @@ param_grid = {
     "C": [1e3, 5e3, 1e4, 5e4, 1e5],
     "gamma": [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1],
     }
-clf = GridSearchCV(SVC(kernel='rbf', class_weight="auto"), param_grid)
+clf = GridSearchCV(SVC(kernel='rbf', class_weight="balanced"), param_grid)
 clf = clf.fit(X_train_pca, Y_train)
 
 print("done in %0.3fs" % (time() - t0))
@@ -88,9 +88,9 @@ def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
 
 
 def title(Y_pred, Y_test, target_names, i):
-    pred_name = target_names[Y_pred[i]].rsplit("", 1)[-1]
-    true_name = target_names[Y_test[i]].rsplit("", 1)[-1]
-    return "predicted: %s" % (pred_name, true_name)
+    pred_name = target_names[Y_pred[i]].rsplit(' ', 1)[-1]
+    true_name = target_names[Y_test[i]].rsplit(' ', 1)[-1]
+    return "predicted: %s\ntrue" % str((pred_name, true_name))
 
 
 prediction_titles = [
